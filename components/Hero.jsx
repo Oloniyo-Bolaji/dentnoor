c1ad645d48e65a51ea307ef235b4e22a229a8813
unknown <user.name>
2025-07-27 09:57:01 +0100

    hero section adjusted

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -5,9 +5,9 @@ const Hero = () => {
   return (
     <header
       style={{ backgroundImage: "url('/hero.png')" }}
-      className=" bg-no-repeat bg-cover bg-center pt-[15px] h-[850px] opacity-100 rotate-0"
+      className="bg-no-repeat bg-center bg-cover pt-[15px] opacity-100 rotate-0 flex justify-between flex-col"
     >
-      <div className="flex-col-center gap-[20px] pt-[10px] w-full h-[433px]">
+      <div className="flex-col-center gap-[20px] pt-[10px] w-full pb-[90px]">
         <h1 className="font-semibold text-[64px] leading-[100%] text-center">
           Start the day With a Bright{" "}
           <span className="block text-center leading-tight">Smile</span>
@@ -17,7 +17,7 @@ const Hero = () => {
           Your AI-powered oral health companion, making preventive dental care
           simple, personal and accessible for everyone.
         </p>
-        <div className="flex gap-[10px]">
+        <div className="flex gap-[10px] justify-center">
           <button className="rounded-[15px] bg-[#009bdb] text-[#ffffff] text-[14px] font-medium w-[200px] h-[40px]">
             <a
               target="_blank"
@@ -32,7 +32,7 @@ const Hero = () => {
           </button>
         </div>
       </div>
-      <div className="flex pr-[500px] pl-[100px] pt-[75px] gap-0">
+      <div className="flex items-end pl-[100px] ">
         <div className="relative w-[380px] h-[360px]">
           <Image
             src="/brush.png"
@@ -42,7 +42,7 @@ const Hero = () => {
           />
         </div>
 
-        <div className="relative top-[-135px] w-[440px] h-[500px]">
+        <div className="relative w-[440px] h-[500px]">
           <Image
             src="/hero-screen.png"
             alt="screen"
